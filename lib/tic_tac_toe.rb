d02f96b8368f448a55e790c1dda1f08566c5c985
derrick b <dbozich1@gmail.com>
2018-05-12 19:48:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,11 +32,27 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board,index)
-    move(board,index)
+    move(board,index,current_player)
     display_board(board)
   else
     turn(board)
   end
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  result = (turn % 2 == 0 ? "X" : "O")
+  result
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  turns
 end
 
 # Helper Method
@@ -91,23 +107,7 @@ def winner(board)
 
 end
 
-def current_player(board)
-  turn = turn_count(board)
-  result = (turn % 2 == 0 ? "X" : "O")
-  result
 
-
-end
-
-def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
-  end
-  turns
-end
 
 def play(board)
   while !over?(board)
@@ -118,7 +118,7 @@ def play(board)
     result = winner(board)
     puts "Congratulations ${result}!"
   elsif draw?(board)
-    puts "It's a draw!" 
+    puts "Cat's Game!"
   end
 
 
